c0ec7d4f243346b156b473094ed940559526141a
cbolles <collinbolles@gmail.com>
2024-04-03 13:59:43 -0400

    Start implementation of sending email

--- packages/email/src/index.ts
+++ packages/email/src/index.ts
@@ -1,38 +1,50 @@
 import { Context, StructuredReturn } from 'faas-js-runtime';
+import { MongoClient, Collection } from 'mongodb';
+import * as dotenv from 'dotenv';
+
+// Configure environment variables
+dotenv.config();
+
+/** Get the email collection from the DB */
+const getEmailCollection = async (): Promise<Collection> => {
+  const mongoURI: string | undefined = process.env.MONGO_URI;
+  if (!mongoURI) {
+    throw new Error(`MONGO_URI not defined`);
+  }
+  const dbName: string | undefined = process.env.MONGO_DB;
+  if (!dbName) {
+    throw new Error(`MONGO_DB not defined`);
+  }
+  const collectionName: string | undefined = process.env.MONGO_COLLECTION;
+  if (!collectionName) {
+    throw new Error(`MONGO_COLLECTION not defined`);
+  }
+
+  const client = new MongoClient(mongoURI);
+  await client.connect();
+  const database = client.db(dbName);
+  return database.collection(collectionName);
+};
 
 /**
- * Your HTTP handling function, invoked with each request. This is an example
- * function that logs the incoming request and echoes its input to the caller.
- *
- * It can be invoked with `func invoke`
- * It can be tested with `npm test`
- *
- * It can be invoked with `func invoke`
- * It can be tested with `npm test`
- *
- * @param {Context} context a context object.
- * @param {object} context.body the request body if any
- * @param {object} context.query the query string deserialized as an object, if any
- * @param {object} context.log logging object with methods for 'info', 'warn', 'error', etc.
- * @param {object} context.headers the HTTP request headers
- * @param {string} context.method the HTTP request method
- * @param {string} context.httpVersion the HTTP protocol version
- * See: https://github.com/knative/func/blob/main/docs/guides/nodejs.md#the-context-object
+ * Send out an email containing the submissions
  */
-const handle = async (context: Context, body: string): Promise<StructuredReturn> => {
-  // YOUR CODE HERE
-  context.log.info(`
------------------------------------------------------------
-Headers:
-${JSON.stringify(context.headers)}
+const handle = async (_context: Context, body: string): Promise<StructuredReturn> => {
+  const emailCollection = await getEmailCollection();
 
-Query:
-${JSON.stringify(context.query)}
+  const submissions = emailCollection.find({ collected: false });
+  const emails: string[] = [];
+  for await (const submission of submissions) {
+    emails.push(submission.email);
+  }
 
-Body:
-${JSON.stringify(body)}
------------------------------------------------------------
-`);
+  // No emails, continue
+  if (emails.length) {
+    return {
+      statusCode: 200
+    };
+  }
+
   return {
     body: body,
     headers: {
